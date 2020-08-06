1765da5a482649b3b93d848007e97609e53a2c66
santexnik1 <sashamochalov2@gmail.com>
2020-08-06 06:37:13 +0400

    DL Academy

---
+++ santexnik.py
@@ -0,0 +1,10 @@
+default_pass = 'x777'
+
+password = input('Vvedite pass -')
+
+if password == default_pass:
+   print('Pass zaebis. Welcome')
+
+if password != default_pass:
+   print('Pass ne tot. Udu naxu`')
+   
